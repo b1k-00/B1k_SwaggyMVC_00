92d50177386df447988294ab686d8051378a824e
BCollins <brennencollins@bears.mybrcc.edu>
2023-03-25 00:42:23 -0500

    Late NIght flights

---
+++ Adventure/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Adventure.Domain
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; } 
+
+
+    }
+}
---
+++ Adventure/IUserApp.cs
@@ -0,0 +1,14 @@
+﻿using Adventure.Domain;
+
+namespace Adventure
+{
+    public interface IUserApp
+    {
+        bool CreateUser(User user);
+
+        bool ValidateUser(string username, string password);
+
+        bool DeleteUser(User user);
+
+    }
+}
